99fab42decae545a5eeb9e7c7f51bf14adef7f61
nato <nlittle315@gmail.com>
2026-04-23 19:32:30 +1000

    26.1

--- src/client/java/dhmi/mixin/client/BundleMouseActionsMixin.java
+++ src/client/java/dhmi/mixin/client/BundleMouseActionsMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(BundleMouseActions.class)
 public class BundleMouseActionsMixin {
     // Invert scrolling direction because items are rendered in the opposite order now
-    @ModifyVariable(method = "onMouseScrolled", at = @At(value = "STORE", ordinal = 0), ordinal = 2)
+    @ModifyVariable(method = "onMouseScrolled", at = @At(value = "STORE", ordinal = 0), name = "wheel")
     private int modifyScrollAmount(int original) {
         return original * -1;
     }
--- src/client/java/dhmi/mixin/client/BundleTooltipMixin.java
+++ src/client/java/dhmi/mixin/client/BundleTooltipMixin.java
@@ -1,20 +1,23 @@
 package dhmi.mixin.client;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientBundleTooltip;
-import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.component.BundleContents;
+import org.apache.commons.lang3.math.Fraction;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.text.DecimalFormat;
-import java.util.List;
 
 @Mixin(ClientBundleTooltip.class)
 public abstract class BundleTooltipMixin {
@@ -22,105 +25,89 @@ public abstract class BundleTooltipMixin {
 	@Unique	private static final int columns = 8;
 	@Unique	private static final DecimalFormat df = new DecimalFormat("#.##");
 
-	@Final
-	@Shadow	private BundleContents contents;
-	@Shadow	private List<ItemStack> getShownItems(int i) {return null;}
-	@Shadow	private int getContentXOffset(int i) {return 0;}
-	@Shadow	private void drawSelectedItemTooltip(Font font, GuiGraphics guiGraphics, int i, int j, int k) {}
-	@Shadow	private void drawProgressbar(int i, int j, Font font, GuiGraphics guiGraphics) {}
-	@Shadow	private int gridSizeY() {return 0;}
-	@Shadow	private int itemGridHeight() {return 0;}
-	@Shadow private static final ResourceLocation SLOT_HIGHLIGHT_BACK_SPRITE = ResourceLocation.withDefaultNamespace("container/bundle/slot_highlight_back");
-	@Shadow private static final ResourceLocation SLOT_HIGHLIGHT_FRONT_SPRITE = ResourceLocation.withDefaultNamespace("container/bundle/slot_highlight_front");
+	@Final @Shadow private BundleContents contents;
 
 	@Inject(method = "slotCount", at = @At("HEAD"), cancellable = true)
 	private void modifySlotCount(CallbackInfoReturnable<Integer> cir) {
 		cir.setReturnValue(this.contents.size());
 	}
 
-	@ModifyConstant(method = {"itemGridHeight"}, constant = @Constant(intValue = 24))
-	private int slotHeight(int original) {
-		return BundleTooltipMixin.slotSize;
-	}
-
-	@ModifyConstant(method = "gridSizeY", constant = @Constant(intValue = 4))
-	private int numColumns(int original) {
-		return BundleTooltipMixin.columns;
-	}
-
-	@ModifyConstant(method = {"getWidth", "getContentXOffset", "drawProgressbar"}, constant = @Constant(intValue = 96))
-	private int tooltipWidth(int original) {
-		return BundleTooltipMixin.slotSize * BundleTooltipMixin.columns;
-	}
-
-	@ModifyConstant(method = "getProgressBarFill", constant = @Constant(intValue = 94))
-	private int barProgress(int original) {
-		return BundleTooltipMixin.columns * BundleTooltipMixin.slotSize - 2;
-	}
-
-	@ModifyConstant(method = "drawProgressbar", constant = @Constant(intValue = 48))
-	private int fillText(int original) {
-		return BundleTooltipMixin.columns * BundleTooltipMixin.slotSize / 2;
-	}
-
 	@ModifyReturnValue(method = "getProgressBarFillText", at = @At("RETURN"))
-	private Component addFillText(Component original) {
+	private static Component addFillText(Component original, Fraction weight) {
 		if (original == null) {
-			var frac = this.contents.weight();
-			double scaling = frac.getDenominator() / 64.;
+			double scaling = weight.getDenominator() / 64.;
 			if (scaling == 0) {
 				return null;
 			}
-			String scaled = df.format(frac.getNumerator() / scaling);
+			String scaled = df.format(weight.getNumerator() / scaling);
 			return Component.literal(scaled + "/64");
 		} else {
 			return original;
 		}
 	}
 
-	/**
-	 * @author TorNato
-	 * @reason easier this way
-	 */
-	@Overwrite
-	private void renderBundleWithItemsTooltip(Font font, int tooltip_x, int tooltip_y, int k, int l, GuiGraphics guiGraphics) {
-		List<ItemStack> list = this.getShownItems(this.contents.getNumberOfItemsToShow());
-		int index = 1;
-
-		for(int row = 0; row < this.gridSizeY(); ++row) {
-			for(int col = 0; col < BundleTooltipMixin.columns; ++col) {
-				if (index > list.size()) {
-					break;
-				}
-				int x = tooltip_x + col * BundleTooltipMixin.slotSize;
-				int y = tooltip_y + row * BundleTooltipMixin.slotSize;
-				this.renderSlot(index, x, y, list, index, font, guiGraphics);
-				++index;
-			}
-		}
-
-		this.drawSelectedItemTooltip(font, guiGraphics, tooltip_x, tooltip_y, k);
-		this.drawProgressbar(tooltip_x + this.getContentXOffset(k), tooltip_y + this.itemGridHeight() + 4, font, guiGraphics);
+	@ModifyExpressionValue(method = "extractBundleWithItemsTooltip", at = @At(value = "CONSTANT", args = "intValue=4", ordinal = 0))
+	private int getColumns(int original) {
+		return columns;
 	}
 
-	/**
-	 * @author TorNato
-	 * @reason easier this way
-	 */
-	@Overwrite
-	private void renderSlot(int i, int j, int k, List<ItemStack> list, int l, Font font, GuiGraphics guiGraphics) {
-		int index = list.size() - i;
-		boolean selected = index == this.contents.getSelectedItem();
-		ItemStack itemStack = list.get(index);
-		if (selected) {
-			guiGraphics.blitSprite(RenderPipelines.GUI_TEXTURED, SLOT_HIGHLIGHT_BACK_SPRITE, j - 4, k - 4, 24, 24);
-		}
+	@ModifyReturnValue(method = "getWidth", at = @At("RETURN"))
+	private int modifyWidth(int original) {
+		return slotSize * columns;
+	}
 
-		guiGraphics.renderItem(itemStack, j, k, l);
-		guiGraphics.renderItemDecorations(font, itemStack, j, k);
+	@ModifyExpressionValue(method = "getProgressBarFill", at = @At(value = "CONSTANT", args = "intValue=94"))
+	private static int barProgress(int original) {
+		return columns * slotSize - 2;
+	}
 
-		if (selected) {
-			guiGraphics.blitSprite(RenderPipelines.GUI_TEXTURED, SLOT_HIGHLIGHT_FRONT_SPRITE, j - 4, k - 4, 24, 24);
-		}
+	@ModifyExpressionValue(method = "extractProgressbar", at = @At(value = "CONSTANT", args = "intValue=48"))
+	private static int fillText(int original) {
+		return columns * slotSize / 2;
+	}
+
+	@ModifyExpressionValue(method = "extractBundleWithItemsTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/component/BundleContents;size()I"))
+	private int modifySize(int original) {
+		return 0;
+	}
+
+	@ModifyExpressionValue(method = "extractProgressbar", at = @At(value = "CONSTANT", args = "intValue=96"))
+	private static int progressBarWidth(int original) {
+		return columns * slotSize;
+	}
+
+	@ModifyExpressionValue(method = "gridSizeY", at = @At(value = "CONSTANT", args = "intValue=4"))
+	private static int gridSizeY(int original) {
+		return columns;
+	}
+
+	@ModifyExpressionValue(method = "itemGridHeight", at = @At(value = "CONSTANT", args = "intValue=24"))
+	private static int gridHeight(int original) {
+		return slotSize;
+	}
+
+	@Definition(id = "drawX", local = @Local(type = int.class, name = "drawX"))
+	@Expression("drawX = @(?)")
+	@ModifyExpressionValue(method = "extractBundleWithItemsTooltip", at = @At("MIXINEXTRAS:EXPRESSION"))
+	private int modifyDrawX(int value, final Font font, final int x, @Local(name = "columnNumber") int columnNumber) {
+		return x + (columnNumber - 1) * slotSize - 4;
+	}
+
+	@Definition(id = "drawY", local = @Local(type = int.class, name = "drawY"))
+	@Expression("drawY = @(?)")
+	@ModifyExpressionValue(method = "extractBundleWithItemsTooltip", at = @At("MIXINEXTRAS:EXPRESSION"))
+	private int modifyDrawY(int value, final Font font, final int x, final int y, @Local(name = "rowNumber") int rowNumber) {
+		return y + (rowNumber - 1) * slotSize - 4;
+	}
+
+	@Definition(id = "blitSprite", method = "Lnet/minecraft/client/gui/GuiGraphicsExtractor;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIII)V")
+	@Definition(id = "SLOT_BACKGROUND_SPRITE", field = "Lnet/minecraft/client/gui/screens/inventory/tooltip/ClientBundleTooltip;SLOT_BACKGROUND_SPRITE:Lnet/minecraft/resources/Identifier;")
+	@Expression("?.blitSprite(?, SLOT_BACKGROUND_SPRITE, ?, ?, ?, ?)")
+	@Redirect(method = "extractSlot", at = @At("MIXINEXTRAS:EXPRESSION"))
+	private void foo(GuiGraphicsExtractor instance, RenderPipeline renderPipeline, Identifier location, int x, int y, int width, int height) {}
+
+	@ModifyExpressionValue(method = "extractBundleWithItemsTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/inventory/tooltip/ClientBundleTooltip;getContentXOffset(I)I"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/inventory/tooltip/ClientBundleTooltip;extractSelectedItemTooltip(Lnet/minecraft/client/gui/Font;Lnet/minecraft/client/gui/GuiGraphicsExtractor;III)V")))
+	private int modifyProgressBarX(int original) {
+		return 0;
 	}
 }
